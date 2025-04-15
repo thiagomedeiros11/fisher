248fc84b6cbd920d427fbb3cb8caa5ca1badee87
Thiago Medeiros <thiago.wav1@gmail.com>
2025-04-15 19:45:10 -0300

    feat: add fisher bot

---
+++ fisher.py
@@ -0,0 +1,103 @@
+import tkinter as tk
+import pyautogui
+import threading
+import time
+import random
+from pynput import mouse, keyboard
+
+water_pos = []
+fishingrod_pos = None
+fishing = False
+pause = False
+water_index = 0
+
+def map_fishingrod_water():
+
+    def on_click(x, y, button, pressed):
+        global fishingrod_pos, water_pos 
+        if not pressed:
+            return
+        if button == mouse.Button.right:
+            fishingrod_pos= (x, y)
+            print(f"[Mapping] Fishing Rod: {fishingrod_pos}")
+        elif button == mouse.Button.left:
+            water_pos.append((x, y))
+            print(f"[Mapping] Water: {x}, {y}")
+
+    def on_press(key):
+        if key == keyboard.Key.esc:
+            print("Leaving mapping mode.")
+            listener_mouse.stop()
+            listener_keyboard.stop()
+
+    listener_mouse = mouse.Listener(on_click=on_click)
+    listener_keyboard = keyboard.Listener(on_press=on_press)
+    listener_mouse.start()
+    listener_keyboard.start()
+
+def loop_fishing():
+    global fishing, pause, water_index
+    while fishing:
+        if pause:
+            time.sleep(0.1)
+            continue
+
+        if not fishingrod_pos or not water_pos:
+            print("Map at least one water position.")
+            break
+
+        pyautogui.moveTo(fishingrod_pos[0], fishingrod_pos[1], duration=random.uniform(0.1, 0.3))
+        pyautogui.click(button='right')
+        print(f"Right click on fishing rod pos: {fishingrod_pos}")
+        time.sleep(random.uniform(0.3, 0.6))
+
+        pos = water_pos[water_index]
+        water_index = (water_index + 1) % len(water_pos)
+
+        pyautogui.moveTo(pos[0], pos[1], duration=random.uniform(0.2, 0.5))
+        time.sleep(random.uniform(0.1, 0.4))
+
+        pyautogui.click(button='left')
+        print(f"Left click on water pos: {pos}")
+
+        time.sleep(random.uniform(4.5, 6.5))
+
+def start_fishing():
+    global fishing
+    if not fishing:
+        fishing = True
+        threading.Thread(target=loop_fishing).start()
+        print("Fishing started!")
+
+def stop_fishing():
+    global pause, fishing
+    pause = True
+    fishing = False
+    print("Bot pause.")
+
+def keep_fishing():
+    global pause, fishing
+    if not pause:
+        return
+    pause = False
+    fishing = True
+    threading.Thread(target=loop_fishing).start()
+    print("Bot resumed.")
+
+def leave():
+    janela.destroy()
+
+janela = tk.Tk()
+janela.title("Fisher")
+janela.geometry("300x300")
+
+tk.Label(janela, text="1. Click to 'Map' and use:\n → Right = fishing rod\n → Left = water\n → ESC = leave").pack(pady=10)
+
+tk.Button(janela, text="Map Fishing Rod + Water", command=map_fishingrod_water).pack(pady=5)
+tk.Button(janela, text="Fishing ON", command=start_fishing).pack(pady=5)
+tk.Button(janela, text="Pause", command=stop_fishing).pack(pady=5)
+tk.Button(janela, text="Resume", command=keep_fishing).pack(pady=5)
+tk.Button(janela, text="Leave", command=leave, fg="white", bg="red").pack(pady=10)
+
+janela.mainloop()
+
